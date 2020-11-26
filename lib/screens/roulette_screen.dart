fb28aba7ba0c83a5428cfb46156ebdd5584594e6
Wiktor ZAając <wiktortechwebdev@gmail.com>
2020-11-26 22:52:52 +0100

    working roulette animation with initState

--- lib/screens/roulette_screen.dart
+++ lib/screens/roulette_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_helper/models/default_text_class.dart';
+
+import '../models/default_text_class.dart';
+import '../widgets/roulette_item.dart';
 
 class RouletteScreen extends StatefulWidget {
   static const routeName = '/roulette';
@@ -8,58 +10,73 @@ class RouletteScreen extends StatefulWidget {
   _RouletteScreenState createState() => _RouletteScreenState();
 }
 
-class _RouletteScreenState extends State<RouletteScreen> with SingleTickerProviderStateMixin {
-
+class _RouletteScreenState extends State<RouletteScreen>
+    with SingleTickerProviderStateMixin {
   Animation _animation;
   AnimationController _animationController;
 
-  @override
-  void initState(){
-    super.initState();
-    _animationController = AnimationController(vsync: this, duration: Duration(seconds:5));
-    _animationController.addListener((){
-      setState(() {
 
-      });
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1), value: 0.25, lowerBound: 0.25, upperBound: 0.5);
+    _animationController.addListener(() {
+      _animationController.isCompleted
+          ? _animationController.forward()
+          : _animationController.isDismissed
+              ? _animationController.forward()
+              : setState(() {});
     });
+
     _animationController.forward();
+  }
+
+  @override
+  dispose(){
+    super.dispose();
+    _animationController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
 
-    _animation = CurvedAnimation(parent: _animationController, curve:Curves.elasticInOut);
-    _animation = Tween(begin: -0.5, end: 0.5).animate(_animation);
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.linear);
+    _animation = Tween(begin:-0.5, end: 0.5).animate(_animation);
 
     return Container(
-        width:queryData.size.width,
-        height: queryData.size.height,
-        child: Center(
-          child:Column(
-            children:<Widget> [
-              Material(
-                borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                elevation: 20.0,
-                color: Colors.cyan.shade900,
-                child: Container(
-                  width: queryData.size.width * 0.5,
-                  height: queryData.size.height * 0.3,
-                  child:DefaultTextWidget(
-                    textContent: 'Roulette',
-                  )
+      width: queryData.size.width,
+      height: queryData.size.height,
+      child: Center(
+          child: Column(
+        children: <Widget>[
+          RotationTransition(
+            turns:_animation,
+            child: Container(
+                width: queryData.size.width * 0.5,
+                height: queryData.size.height * 0.3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100.00),
                 ),
-              ),
-              Transform(
-                alignment: FractionalOffset(0.5,0.0),
-                transform: Matrix4.rotationZ(_animation.value),
-                child: Container(
-                  child:Text("!!!!!!!"),
-                ),
-              )
-            ],
-          )
-        ),
+                child: Row(
+                  children: <Widget>[
+                    RouletteItem('X2', Colors.black, BorderRadius.only(topLeft:Radius.circular(100),bottomLeft:Radius.circular(100)),),
+                    RouletteItem('X2', Colors.red, BorderRadius.only(topRight:Radius.circular(100),bottomRight:Radius.circular(100)),),
+                  ],
+                )),
+          ),
+          // Transform(
+          //   alignment: FractionalOffset(0.5,0.0),
+          //   transform: Matrix4.rotationZ(_animation.value),
+          //   child: Container(
+          //     child:Text("!!!!!!!"),
+          //   ),
+          // )
+        ],
+      )),
     );
   }
 }
---
+++ lib/widgets/roulette_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../models/default_text_class.dart';
+
+class RouletteItem extends StatelessWidget {
+
+  final String text;
+  final Color bgColor;
+  final BorderRadius borderRadius;
+
+  RouletteItem(this.text,this.bgColor,this.borderRadius);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData = MediaQuery.of(context);
+
+    return Expanded(
+      child: Container(
+        height:queryData.size.height * 0.3,
+        decoration: BoxDecoration(
+          borderRadius: borderRadius,
+          color: bgColor,
+        ),
+        child: Center(
+          child: DefaultTextWidget(
+            textContent:text,
+            fontColor: Colors.white,
+            isFittedBox:false,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
