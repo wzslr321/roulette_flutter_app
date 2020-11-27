32bdcfcddd97b95c2feb75a94e149f92afaa39a0
Wiktor ZAając <wiktortechwebdev@gmail.com>
2020-11-27 14:48:34 +0100

    prototype of roulette function responsible for checking spinned color

--- lib/screens/roulette_screen.dart
+++ lib/screens/roulette_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 import '../widgets/roulette_item.dart';
+import '../models/default_text_class.dart';
 
 class RouletteScreen extends StatefulWidget {
   static const routeName = '/roulette';
@@ -14,16 +16,25 @@ class _RouletteScreenState extends State<RouletteScreen>
   Animation _animation;
   AnimationController _animationController;
 
+  bool didWin;
+  bool didEnd;
+  bool isRedOnTween;
+
 
   void _roll() {
     _animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 10), value: 0.0, lowerBound: 0.0, upperBound:1.0);
+        AnimationController(vsync: this, duration: const Duration(seconds: 10), value: 0.1, lowerBound: 0.0, upperBound:1.0, );
     _animationController.addListener(() {
-      _animationController.isCompleted
-          ? _animationController.forward()
-          : _animationController.isDismissed
-          ? _animationController.forward()
-          : setState(() {});
+      if(_animationController.isCompleted){
+        _animationController.forward();
+        setState(() {
+          didEnd = true;
+        });
+      } else {
+        _animationController.isDismissed
+            ? _animationController.forward()
+            : setState(() {});
+      }
     });
 
     _animationController.forward();
@@ -43,13 +54,25 @@ class _RouletteScreenState extends State<RouletteScreen>
     _animationController.dispose();
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
-
+    double tweenEnd = 0;
+    double finalTween;
+    tweenEnd =  new Random().nextInt(100) / 10 + 20;
+    if(didEnd == true) {
+      finalTween = tweenEnd;
+      print(finalTween);
+      String convertedTween = finalTween.toString();
+      int.parse(convertedTween[3])  < 5 ? isRedOnTween = true : isRedOnTween = false;
+      print(convertedTween[3]);
+      print(isRedOnTween);
+    }
     _animation =
         CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
-    _animation = Tween(begin:-1.0, end: 51.0).animate(_animation);
+    _animation = Tween(begin:-1.0, end: tweenEnd).animate(_animation);
 
     return Container(
       width: queryData.size.width,
@@ -76,6 +99,12 @@ class _RouletteScreenState extends State<RouletteScreen>
             onTap:_roll,
             child:Container(
               child: Text("tap"),
+            )
+          ),
+          Container(
+            color:Colors.green,
+            child:DefaultTextWidget(
+                textContent: didWin != null ? 'You won!' : 'You lost',
             )
           )
           // Transform(
